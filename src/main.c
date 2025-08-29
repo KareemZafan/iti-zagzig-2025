83999d0e45cdf65647c855a97b0688ace37292d9
KareemZafan <kareemm.zafan@gmail.com>
2025-08-29 12:17:22 +0300

    add a new subtraction function

--- src/main.c
+++ src/main.c
@@ -4,12 +4,19 @@ double add (double a, double b){
 return a + b; 
 }
 
+double sub (double a, double b){
+return a - b; 
+}
+
+
 int main(void){
 
 int a = 5; 
 int b = 10; 
 
 printf("Sum = %d",add(a,b));  
+printf("Sub = %d",sub(a,b));  
+
 
 return 0; 
 }

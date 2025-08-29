00dc844539b82b52c21f2ca7bcb2ab0983819f5b
KareemZafan <kareemm.zafan@gmail.com>
2025-08-29 12:11:58 +0300

    Adding a new function

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+double add (double a, double b){
+return a + b; 
+}
 
 int main(void){
 
 int a = 5; 
 int b = 10; 
 
-printf("Sum = %d",a + b);  
+printf("Sum = %d",add(a,b));  
 
 return 0; 
 }
